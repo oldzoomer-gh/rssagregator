daf1ff2b67ef9e7d3a8d6565321e389100aadbce
Egor Gavrilov <gavrilovegor519@gmail.com>
2024-02-29 13:08:07 +0300

    I'm now not including test mode with H2 DB.

--- backend/src/test/java/com/egor/rssaggregator/RssaggregatorApplicationTests.java
+++ backend/src/test/java/com/egor/rssaggregator/RssaggregatorApplicationTests.java
@@ -2,10 +2,8 @@ package com.egor.rssaggregator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource("/application-testing.properties")
 class RssaggregatorApplicationTests {
 
 	@Test
