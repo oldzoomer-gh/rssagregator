8b2ac6b8465d6d0c1a88b107c79c24eff61de4fd
Egor Gavrilov <gavrilovegor519@gmail.com>
2024-03-05 22:48:38 +0300

    Create feed service.

--- backend/src/main/java/com/egor/rssaggregator/RssaggregatorApplication.java
+++ backend/src/main/java/com/egor/rssaggregator/RssaggregatorApplication.java
@@ -1,13 +1,38 @@
 package com.egor.rssaggregator;
 
+import com.google.common.cache.CacheBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
+@EnableCaching
 public class RssaggregatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RssaggregatorApplication.class, args);
+	}
+
+	@Bean
+	public CacheManager cacheManager() {
+		return new ConcurrentMapCacheManager() {
+			@Override
+            protected Cache createConcurrentMapCache(String name) {
+				return new ConcurrentMapCache(
+						name,
+						CacheBuilder.newBuilder()
+								.expireAfterWrite(15, TimeUnit.MINUTES)
+								.build().asMap(),
+						false);
+			}
+		};
 	}
 
 }
--- backend/src/main/java/com/egor/rssaggregator/service/impl/FeedServiceImpl.java
+++ backend/src/main/java/com/egor/rssaggregator/service/impl/FeedServiceImpl.java
@@ -1,6 +1,101 @@
 package com.egor.rssaggregator.service.impl;
 
+import com.egor.rssaggregator.dto.input.AddFeedDto;
+import com.egor.rssaggregator.dto.output.GetFeedDto;
+import com.egor.rssaggregator.dto.output.MainNewsEntryDto;
+import com.egor.rssaggregator.entity.Feed;
+import com.egor.rssaggregator.entity.User;
+import com.egor.rssaggregator.exception.UserNotFound;
+import com.egor.rssaggregator.mapper.input.FeedInputMapper;
+import com.egor.rssaggregator.mapper.output.FeedOutputMapper;
+import com.egor.rssaggregator.repo.UserRepo;
 import com.egor.rssaggregator.service.FeedService;
+import com.egor.rssaggregator.util.GetFeed;
+import lombok.AllArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+@Service
+@AllArgsConstructor
 public class FeedServiceImpl implements FeedService {
+    private final FeedInputMapper feedInputMapper;
+    private final FeedOutputMapper feedOutputMapper;
+    private final UserRepo userRepo;
+
+    @Override
+    public void addFeed(AddFeedDto addFeedDto, String email) throws UserNotFound {
+        Feed feed = feedInputMapper.toEntity(addFeedDto);
+
+        User user = userRepo.findByEmail(email)
+                .orElseThrow(() -> new UserNotFound("User not found!"));
+
+        user.getFeeds().add(feed);
+
+        userRepo.save(user);
+    }
+
+    @Override
+    public List<GetFeedDto> getFeeds(String email) throws UserNotFound {
+        User user = userRepo.findByEmail(email)
+                .orElseThrow(() -> new UserNotFound("User not found!"));
+
+        return feedOutputMapper.toDto(user.getFeeds());
+    }
+
+    @Override
+    public void deleteFeed(long id, String email) throws UserNotFound {
+        User user = userRepo.findByEmail(email)
+                .orElseThrow(() -> new UserNotFound("User not found!"));
+
+        user.getFeeds().removeIf(feed -> feed.getId() == id);
+
+        userRepo.save(user);
+    }
+
+    @Override
+    @Cacheable(value = "mainNews", key = "#email")
+    public Page<MainNewsEntryDto> getNewsHeadings(String email, Pageable pageable)
+            throws UserNotFound, ExecutionException, InterruptedException {
+        User user = userRepo.findByEmail(email)
+                .orElseThrow(() -> new UserNotFound("User not found!"));
+        List<Feed> feeds = user.getFeeds();
+
+        var newsEntries = new ArrayList<MainNewsEntryDto>();
+
+        for (Feed feed : feeds) {
+            String url = feed.getUrl();
+
+            var news = GetFeed.getFeed(url)
+                    .getEntries()
+                    .stream()
+                    .map(entry -> {
+                        var title = entry.getTitle();
+                        var link = entry.getLink();
+                        var date = entry.getPublishedDate();
+
+                        var mainNewsEntry = new MainNewsEntryDto();
+                        mainNewsEntry.setNewsHead(title);
+                        mainNewsEntry.setFeedUrl(link);
+                        mainNewsEntry.setNewsDate(date.toInstant().
+                                atZone(ZoneId.systemDefault())
+                                .toLocalDateTime());
+
+                        return mainNewsEntry;
+                    }).toList();
+
+            newsEntries.addAll(news);
+        }
+
+        int start = (int) pageable.getOffset();
+        int end = Math.min(start + pageable.getPageSize(), newsEntries.size());
+        return new PageImpl<>(newsEntries.subList(start, end), pageable, newsEntries.size());
+    }
 }
