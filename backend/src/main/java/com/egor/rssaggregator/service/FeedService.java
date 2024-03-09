ed74fe0d0fee6cf7fe528dd332d9e8d00145a5dd
Egor Gavrilov <gavrilovegor519@gmail.com>
2024-03-09 20:14:45 +0300

    Adding new JavaDocs.

--- backend/src/main/java/com/egor/rssaggregator/service/FeedService.java
+++ backend/src/main/java/com/egor/rssaggregator/service/FeedService.java
@@ -8,12 +8,37 @@ import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
+/**
+ * Management of feeds service layer.
+ * @author Egor Gavrilov (gavrilovegor519@gmail.com)
+ */
 public interface FeedService {
+    /**
+     * Add new feed.
+     * @param addFeedDto Add feed DTO.
+     * @param email Email of user who added feed.
+     */
     void addFeed(AddFeedDto addFeedDto, String email);
 
+    /**
+     * Get all feeds.
+     * @param email Email of user who requested feeds.
+     * @return List of feeds.
+     */
     List<GetFeedDto> getFeeds(String email);
 
+    /**
+     * Delete feed.
+     * @param id Id of feed.
+     * @param email Email of user who deleted feed.
+     */
     void deleteFeed(long id, String email);
 
+    /**
+     * Get news headings.
+     * @param email Email of user who requested news.
+     * @param pageable Pageable object.
+     * @return List of news headings.
+     */
     Page<MainNewsEntryDto> getNewsHeadings(String email, Pageable pageable);
 }
--- backend/src/main/java/com/egor/rssaggregator/service/UserService.java
+++ backend/src/main/java/com/egor/rssaggregator/service/UserService.java
@@ -4,8 +4,22 @@ import com.egor.rssaggregator.dto.input.LoginDto;
 import com.egor.rssaggregator.dto.input.RegDto;
 import com.egor.rssaggregator.dto.output.TokenDto;
 
+/**
+ * User management service layer. User can login and register.
+ * @author Egor Gavrilov (gavrilovegor519@gmail.com)
+ */
 public interface UserService {
+
+    /**
+     * Login user and return JWT Bearer token.
+     * @param loginDto Login data
+     * @return JWT Bearer token
+     */
     TokenDto login(LoginDto loginDto);
 
+    /**
+     * Register new user.
+     * @param regDto Registration data
+     */
     void reg(RegDto regDto);
 }
