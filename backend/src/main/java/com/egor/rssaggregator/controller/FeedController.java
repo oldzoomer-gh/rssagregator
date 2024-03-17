02e3ce2af94369c991a189b90a84f436f5405b2e
Egor Gavrilov <gavrilovegor519@gmail.com>
2024-03-17 19:47:52 +0300

    Add Swagger docs.

--- backend/src/main/java/com/egor/rssaggregator/controller/FeedController.java
+++ backend/src/main/java/com/egor/rssaggregator/controller/FeedController.java
@@ -3,6 +3,9 @@ package com.egor.rssaggregator.controller;
 import com.egor.rssaggregator.dto.FeedDto;
 import com.egor.rssaggregator.dto.NewsEntryDto;
 import com.egor.rssaggregator.service.FeedService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -21,25 +24,61 @@ public class FeedController {
     private final FeedService feedService;
 
     @PostMapping("/add")
-    public void addFeed(@RequestBody @Valid FeedDto dto,
+    @Operation(summary = "Add new feed",
+            responses = {
+                    @ApiResponse(description = "Feed is added",
+                            responseCode = "200"),
+                    @ApiResponse(responseCode = "409",
+                            description = "Duplicate feed data"),
+                    @ApiResponse(responseCode = "403",
+                            description = "User is not authenticated")
+            })
+    public void addFeed(@Parameter(description = "Feed data", required = true)
+                            @RequestBody @Valid FeedDto dto,
                         Authentication authentication) {
         feedService.addFeed(dto, authentication.getName());
     }
 
     @GetMapping("/list")
+    @Operation(summary = "Add new feed",
+            responses = {
+                    @ApiResponse(description = "List of feeds is returned",
+                            responseCode = "200"),
+                    @ApiResponse(responseCode = "403",
+                            description = "User is not authenticated")
+            })
     public List<FeedDto> listFeeds(Authentication authentication) {
         return feedService.getFeeds(authentication.getName());
     }
 
     @DeleteMapping("/delete/{id}")
-    public void deleteFeed(@PathVariable long id,
+    @Operation(summary = "Add new feed",
+            responses = {
+                    @ApiResponse(description = "Feed is deleted",
+                            responseCode = "200"),
+                    @ApiResponse(responseCode = "403",
+                            description = "User is not authenticated")
+            })
+    public void deleteFeed(@Parameter(description = "Feed id", required = true)
+                               @PathVariable long id,
                            Authentication authentication) {
         feedService.deleteFeed(id, authentication.getName());
     }
 
     @GetMapping("/getNewsFromAllFeeds")
+    @Operation(summary = "Add new feed",
+            responses = {
+                    @ApiResponse(description = "The list of news is returned",
+                            responseCode = "200"),
+                    @ApiResponse(responseCode = "400",
+                            description = "Incorrect request"),
+                    @ApiResponse(responseCode = "403",
+                            description = "User is not authenticated")
+            })
     public Page<NewsEntryDto> getNewsFromAllFeeds(Authentication authentication,
+                                                  @Parameter(description = "Page number", required = true)
                                                   @RequestParam(defaultValue = "0") int page,
+                                                  @Parameter(description = "Page size", required = true)
                                                   @RequestParam(defaultValue = "10") int size) {
         var pageable = PageRequest.of(page, size);
         return feedService.getNewsHeadings(authentication.getName(), pageable);
--- backend/src/main/java/com/egor/rssaggregator/controller/UserController.java
+++ backend/src/main/java/com/egor/rssaggregator/controller/UserController.java
@@ -3,6 +3,9 @@ package com.egor.rssaggregator.controller;
 import com.egor.rssaggregator.dto.TokenDto;
 import com.egor.rssaggregator.dto.UserDto;
 import com.egor.rssaggregator.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,12 +20,28 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/login")
-    public TokenDto login(@RequestBody @Valid UserDto userDto) {
+    @Operation(summary = "Get JWT token by login and password",
+            responses = {
+                    @ApiResponse(description = "JWT token for user",
+                            useReturnTypeSchema = true),
+                    @ApiResponse(responseCode = "403",
+                            description = "User is not found or incorrect password")
+            })
+    public TokenDto login(@Parameter(required = true, description = "User data")
+                              @RequestBody @Valid UserDto userDto) {
         return userService.login(userDto);
     }
 
     @PostMapping("/reg")
-    public void reg(@RequestBody @Valid UserDto userDto) {
+    @Operation(summary = "Register the user for using this service",
+            responses = {
+                    @ApiResponse(description = "User is registered",
+                            responseCode = "200"),
+                    @ApiResponse(responseCode = "409",
+                            description = "Duplicate registration data")
+            })
+    public void reg(@Parameter(required = true, description = "User data")
+                        @RequestBody @Valid UserDto userDto) {
         userService.reg(userDto);
     }
 }
